3895555fc60681bb99e383be0009b5f568586b38
dquangminh <minhdqhe170267@fpt.edu.vn>
2026-03-01 21:42:09 +0700

    feat(training): M TrainingExerciseRepository, RoadmapExerciseRepository,TrainingMethodRepository,TrainingRoadmapRepository

--- backend/src/main/java/vn/edu/fpt/doghandbook/backend/repository/TrainingRoadmapRepository.java
+++ backend/src/main/java/vn/edu/fpt/doghandbook/backend/repository/TrainingRoadmapRepository.java
@@ -1,7 +1,19 @@
 package vn.edu.fpt.doghandbook.backend.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import vn.edu.fpt.doghandbook.backend.entity.TrainingRoadmap;
 
+import java.util.List;
+
 public interface TrainingRoadmapRepository extends JpaRepository<TrainingRoadmap, Integer> {
+
+    Page<TrainingRoadmap> findByIsDeletedFalse(Pageable pageable);
+
+    @Query("SELECT tr FROM TrainingRoadmap tr WHERE tr.isDeleted = false "
+            + "AND tr.dogBreed.breedId = :breedId")
+    List<TrainingRoadmap> findByBreedBreedIdAndIsDeletedFalse(@Param("breedId") Integer breedId);
 }
