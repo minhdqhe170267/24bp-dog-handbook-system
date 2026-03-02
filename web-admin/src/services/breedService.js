781b945c26a21fb3eb6302ecb3f749d0c4cebe8d
phongdinh04 <phongdhhe181925@fpt.edu.vn>
2026-03-02 15:17:29 +0700

    feat: add breed service with CRUD and compare API calls

---
+++ web-admin/src/services/breedService.js
@@ -0,0 +1,12 @@
+import api from './api';
+
+export const breedService = {
+  getAll: (page = 0, size = 10, search = '') =>
+    api.get(`/breeds?page=${page}&size=${size}&search=${search}`),
+  getById: (id) => api.get(`/breeds/${id}`),
+  create: (data) => api.post('/breeds', data),
+  update: (id, data) => api.put(`/breeds/${id}`, data),
+  delete: (id) => api.delete(`/breeds/${id}`),
+  compare: (breedIds) => api.post('/breeds/compare', { breedIds }),
+  getStages: (id) => api.get(`/breeds/${id}/development-stages`),
+};
