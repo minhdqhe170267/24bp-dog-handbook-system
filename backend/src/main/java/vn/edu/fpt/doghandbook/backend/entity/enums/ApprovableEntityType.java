e2a48a4ac7ecc2c3ae9daa28c8382604c201340b
dquangminh <minhdqhe170267@fpt.edu.vn>
2026-03-19 09:57:20 +0700

    feat(Approval-fix): A ApprovableEntityType

---
+++ backend/src/main/java/vn/edu/fpt/doghandbook/backend/entity/enums/ApprovableEntityType.java
@@ -0,0 +1,14 @@
+package vn.edu.fpt.doghandbook.backend.entity.enums;
+
+public enum ApprovableEntityType {
+    CONTENT,
+    DOG_BREED,
+    NUTRITION_STANDARD,
+    TRAINING_EXERCISE,
+    TRAINING_ROADMAP,
+    TRAINING_METHOD,
+    DEVELOPMENT_STAGE,
+    DISEASE,
+    MEDICATION,
+    FIRST_AID_GUIDE
+}
